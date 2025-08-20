e61096ad982cf67b0797ad26bbe3237ae7905ac8
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-08-20 14:55:03 -0400

    it opens, it closes, it looks decent (Basic UI done for now)

--- phosphor/src/main.cpp
+++ phosphor/src/main.cpp
@@ -9,16 +9,21 @@ int main() {
     int offset = 50;
     InitWindow(width + offset, height + offset, "phosphor");
 
+    // Load font
+    Font font = LoadFont("assets/fonts/Debrosee-ALPnL.ttf");
+
     while(!WindowShouldClose()) {
         BeginDrawing();
+            // Base UI - will probably change in the future, it burns my eyes a bit
             ClearBackground(Palette::DarkBg);
-            DrawRectangleRoundedLinesEx({10, 10, 780, 780}, 0.18f, 20, 2, Palette::BrightGreen);
-            DrawLineEx({25, 60}, {775, 60}, 3, Palette::BrightGreen);
-            DrawTextEx();
-        
+            DrawRectangleRoundedLinesEx({10, 10, 780, 780}, 0.18f, 20, 2, Palette::AquaMint);
+            DrawLineEx({25, 60}, {775, 60}, 3, Palette::AquaMint);
+            DrawTextEx(font, "phosphor", {40, 35}, 25.0f, 2, Palette::SoftMint);
         EndDrawing();
     }
 
+    // Unload resources to prevent mem leaks
+    UnloadFont(font);
     CloseWindow();
     return 0;
 }
