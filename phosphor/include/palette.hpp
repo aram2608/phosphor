973e98ab2a652a589a8db554a17f77fe97d26f72
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-08-20 14:46:21 -0400

    basic UI, added pallette.hpp (Keeps the main file clean while making the colors usage consistent)

---
+++ phosphor/include/palette.hpp
@@ -0,0 +1,13 @@
+#include <raylib.h>
+
+// Namespace for color pallete
+namespace Palette {
+    constexpr Color DarkBg      = { 0x0d, 0x0f, 0x0a, 255 };
+    constexpr Color SoftMint    = { 0xb8, 0xff, 0xb8, 255 };
+    constexpr Color BrightGreen = { 0x5c, 0xff, 0x5c, 255 };
+    constexpr Color AquaMint    = { 0x7e, 0xf9, 0xd8, 255 };
+    constexpr Color LightGreen  = { 0xa0, 0xff, 0xa0, 255 };
+    constexpr Color ForestMoss  = { 0x77, 0xc7, 0x77, 255 };
+    constexpr Color LimeYellow  = { 0xcc, 0xff, 0x66, 255 };
+    constexpr Color DeepFern    = { 0x1a, 0x2a, 0x16, 255 };
+}
--- phosphor/src/main.cpp
+++ phosphor/src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <raylib.h>
+#include "palette.hpp"
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    // Harded screen params with offset
+    int width = 750;
+    int height = 750;
+    int offset = 50;
+    InitWindow(width + offset, height + offset, "phosphor");
+
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(Palette::DarkBg);
+            DrawRectangleRoundedLinesEx({10, 10, 780, 780}, 0.18f, 20, 2, Palette::BrightGreen);
+            DrawLineEx({25, 60}, {775, 60}, 3, Palette::BrightGreen);
+            DrawTextEx();
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
